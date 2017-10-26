3745f5dc393cafbf5fcdad799e70239b19416dcd
iuniX <guilaudensack@hotmail.com>
2017-10-26 08:47:55 -0200

    # Revision 1.0 Elevator Window
    Modificações para os botões do elevador

--- game_lugia_elevator/lugia_elevator.lua
+++ game_lugia_elevator/lugia_elevator.lua
@@ -5,9 +5,17 @@ local p_elevatorWindow
 local p_unlockedFloors
 local p_currentFloor
 
+local floorNumbers = 5
+
 function init()
   p_elevatorWindow = g_ui.loadUI('lugia_elevator', rootWidget)
   g_game.handleExtended(elevatorOpCode, _onRecieveOpCode)
+
+  for i = 1, floorNumbers do
+    local button = p_elevatorWindow:getChildById('number'.. i)
+    button:setImageSource("images/button_off")
+  end
+
 end
 
 function terminate()
@@ -15,20 +23,29 @@ function terminate()
   p_elevatorWindow:destroy()
 end
 
-function show()
+function show(i)
   p_elevatorWindow:show()
   p_elevatorWindow:raise()
   p_elevatorWindow:focus()
+  local button = p_elevatorWindow:getChildById('number'.. i)
+  button:setImageSource("images/button_on")
+
 end
 
 function hide()
   p_elevatorWindow:hide()
+
+  for i = 1, floorNumbers do
+    local button = p_elevatorWindow:getChildById('number'.. i)
+    button:setImageSource("images/button_off")
+  end
+
 end
 
 function _onRecieveOpCode(t)
   p_unlockedFloors = t.unlockedFloors
   p_currentFloor = t.currentFloor
-  show()
+  show(p_currentFloor)
 end
 
 function onClickButton(t)
